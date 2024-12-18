6c7a0d1122238d4c52c49483155410d893614fae
ArdakIOS <ardaktursunbayev4@gmail.com>
2024-12-18 18:45:03 +0500

    Initial Commit

---
+++ ResumeBuilder/ResumeBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeBuilderApp.swift
+//  ResumeBuilder
+//
+//  Created by Ardak Tursunbayev on 18.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ResumeBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
